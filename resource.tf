15f1226ed24c278b0b0b7d473867d350083d3382
ash2code <ashoksoma95@gmail.com>
2024-05-17 21:12:53 +0530

    add html files to git

--- resource.tf
+++ resource.tf
@@ -42,4 +42,24 @@ resource "aws_s3_bucket_website_configuration" "s3-bucket-website" {
     }
 }
 
+resource "aws_s3_bucket_policy" "s3-bucket-policy" {
+    bucket = aws_s3_bucket.s3-bucket.id
+    policy = jsonencode({
+        Version = "2012-10-17"
+        Statement = [
+            {
+                Effect = "Allow"
+                Principal = "*"
+                Action = [
+                    "s3:GetObject",
+                    "s3.PutObject",
+                ],
+                Resource = "arn::aws:s3:::${aws_s3_bucket.s3_bucket.id}/*",
+            },
+        ],
+    })
+}
+
+        
+
 
